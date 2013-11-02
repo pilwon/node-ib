8ec0c1cfb623e6f6e130876bd5707280b4720d54
Pilwon Huh <pilwon@gmail.com>
2013-11-02 16:30:50 -0400

    Added reqMktData example.

---
+++ examples/reqMktData.js
@@ -0,0 +1,41 @@
+/*
+ * examples/market-data.js
+ */
+
+'use strict';
+
+var util = require('util');
+
+require('colors');
+
+var ib = new (require('..'))({
+  // clientId: 0,
+  // host: '127.0.0.1',
+  // port: 7496
+}).on('error', function (err) {
+  console.error(util.format('ERROR: %s', err.message).red);
+}).on('tickPrice', function (tickerId, tickType, price, canAutoExecute) {
+  console.log(
+    '%s tickerId=%s, price=%s, canAutoExecute=%s',
+    util.format('[%s]', ib.util.tickTypeToString(tickType)).cyan,
+    tickerId,
+    price,
+    !!canAutoExecute
+  );
+}).on('tickSize', function (tickerId, sizeTickType, size) {
+  console.log(
+    '%s tickerId=%s, size=%s',
+    util.format('[%s]', ib.util.tickTypeToString(sizeTickType)).cyan,
+    tickerId,
+    size
+  );
+});
+
+ib.connect();
+
+ib.reqMktData(1, {
+  currency: 'USD',
+  exchange: 'IDEALPRO',
+  secType: 'CASH',
+  symbol: 'EUR'
+}, '', false);
