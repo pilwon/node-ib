ca35a7672b40727e190d41291d53c48aceec1b02
Pilwon Huh <pilwon@gmail.com>
2013-11-03 01:57:51 -0500

    Added placeOrder example.

---
+++ examples/placeOrder.js
@@ -0,0 +1,88 @@
+/*
+ * examples/placeOrder.js
+ */
+
+'use strict';
+
+require('colors');
+
+var _ = require('lodash');
+
+var ib = new (require('..'))({
+  // clientId: 0,
+  // host: '127.0.0.1',
+  // port: 7496
+}).on('error', function (err) {
+  console.error(err.message.red);
+}).on('result', function (event, args) {
+  if (!_.contains(['nextValidId', 'openOrder', 'openOrderEnd', 'orderStatus'], event)) {
+    console.log('%s %s', (event + ':').yellow, JSON.stringify(args));
+  }
+}).on('nextValidId', function (orderId) {
+  console.log(
+    '%s %s%d',
+    '[nextValidId]'.cyan,
+    'orderId='.bold, orderId
+  );
+}).on('openOrder', function (orderId, contract, order, orderState) {
+  console.log(
+    '%s %s%d %s%s %s%s %s%s',
+    '[openOrder]'.cyan,
+    'orderId='.bold, orderId,
+    'contract='.bold, JSON.stringify(contract),
+    'order='.bold, JSON.stringify(order),
+    'orderState='.bold, JSON.stringify(orderState)
+  );
+}).on('openOrderEnd', function () {
+  console.log('[openOrderEnd]'.cyan);
+}).on('orderStatus', function (id, status, filled, remaining, avgFillPrice, permId,
+                               parentId, lastFillPrice, clientId, whyHeld) {
+  console.log(
+    '%s %s%d %s%s %s%d %s%d %s%d %s%d %s%d %s%d %s%d %s%s',
+    '[orderStatus]'.cyan,
+    'id='.bold, id,
+    'status='.bold, status,
+    'filled='.bold, filled,
+    'remaining='.bold, remaining,
+    'avgFillPrice='.bold, avgFillPrice,
+    'permId='.bold, permId,
+    'parentId='.bold, parentId,
+    'lastFillPrice='.bold, lastFillPrice,
+    'clientId='.bold, clientId,
+    'whyHeld='.bold, whyHeld
+  );
+});
+
+ib.connect();
+
+ib.reqIds(1);
+
+ib.once('nextValidId', function (orderId) {
+  console.log('Placing orders...'.yellow);
+
+  // Place orders
+  ib.placeOrder(orderId, ib.contract.stock('AAPL'), ib.order.limit('BUY', 1, 0.01));
+  ib.placeOrder(orderId + 1, ib.contract.stock('GOOG'), ib.order.limit('SELL', 1, 9999));
+  ib.placeOrder(orderId + 2, ib.contract.stock('FB'), ib.order.limit('BUY', 1, 0.01));
+
+  // Check open orders
+  ib.reqOpenOrders();
+
+  // Check next orderId
+  ib.reqIds(1);
+
+  // Cancel orders after 5 seconds.
+  setTimeout(function () {
+    console.log('Cancelling orders...'.yellow);
+    ib.cancelOrder(orderId);
+    ib.cancelOrder(orderId + 1);
+    ib.cancelOrder(orderId + 2);
+
+    ib.once('openOrderEnd', function () {
+      console.log('Disconnecting...'.yellow);
+      ib.disconnect();
+    });
+
+    ib.reqAllOpenOrders();
+  }, 5000);
+});
