fec30fd8fbc4ca476aa45c957875258e4cdba7f6
Pilwon Huh <pilwon@gmail.com>
2013-11-03 00:18:27 -0400

    Added reqPositions example.

---
+++ examples/reqPositions.js
@@ -0,0 +1,40 @@
+/*
+ * examples/reqPositions.js
+ */
+
+'use strict';
+
+require('colors');
+
+var _ = require('lodash');
+
+var ib = new (require('..'))({
+  // clientId: 0,
+  // host: '127.0.0.1',
+  // port: 7496
+}).on('error', function (err) {
+  console.error(err.message.red);
+}).on('result', function (event, args) {
+  if (!_.contains(['position', 'positionEnd'], event)) {
+    console.log('%s %s', (event + ':').yellow, JSON.stringify(args));
+  }
+}).on('position', function (account, contract, pos, avgCost) {
+  console.log(
+    '%s %s%s %s%s %s%s %s%s',
+    '[position]'.cyan,
+    'account='.bold, account,
+    'contract='.bold, JSON.stringify(contract),
+    'pos='.bold, pos,
+    'avgCost='.bold, avgCost
+  );
+}).on('positionEnd', function () {
+  console.log('%s', '[positionEnd]'.cyan);
+});
+
+ib.connect();
+
+ib.reqPositions();
+
+ib.on('positionEnd', function () {
+  ib.disconnect();
+});
