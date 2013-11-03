60e470b7182e6eab0b98c1f64cdd16006b552d31
Pilwon Huh <pilwon@gmail.com>
2013-11-02 20:07:19 -0400

    Added reqRealTimeBars example.

---
+++ examples/reqRealTimeBars.js
@@ -0,0 +1,86 @@
+/*
+ * examples/reqRealTimeBars.js
+ */
+
+'use strict';
+
+var util = require('util');
+
+require('colors');
+
+var _ = require('lodash');
+
+var CONTRACTS = {};
+
+var ib = new (require('..'))({
+  // clientId: 0,
+  // host: '127.0.0.1',
+  // port: 7496
+}).on('error', function (err) {
+  console.error(util.format('ERROR: %s', err.message).red);
+}).on('result', function (event, args) {
+  if (!_.contains(['realtimeBar'], event)) {
+    console.log('%s %s', (event + ':').yellow, JSON.stringify(args));
+  }
+}).on('realtimeBar', function (reqId, time, open, high, low, close, volume, wap, count) {
+  console.log(
+    '%s reqId=%d time=%d open=%f high=%f low=%f close=%f volume=%d wap=%f count=%d',
+    '[realtimeBar]'.cyan,
+    reqId,
+    time,
+    open,
+    high,
+    low,
+    close,
+    volume,
+    wap,
+    count
+  );
+});
+
+ib.connect();
+
+// Forex
+// ib.reqRealTimeBars(1, ib.contract.forex('EUR'), 5, 'TRADES', false);
+// ib.reqRealTimeBars(2, ib.contract.forex('GBP'), 5, 'BID', false);
+// ib.reqRealTimeBars(3, ib.contract.forex('CAD'), 5, 'ASK', false);
+// ib.reqRealTimeBars(4, ib.contract.forex('HKD'), 5, 'MIDPOINT', false);
+// ib.reqRealTimeBars(5, ib.contract.forex('JPY'), 5, 'TRADES', false);
+// ib.reqRealTimeBars(6, ib.contract.forex('KRW'), 5, 'BID', false);
+
+// Stock
+ib.reqRealTimeBars(11, ib.contract.stock('AAPL'), 5, 'TRADES', false);
+ib.reqRealTimeBars(12, ib.contract.stock('AMZN'), 5, 'BID', false);
+ib.reqRealTimeBars(13, ib.contract.stock('GOOG'), 5, 'ASK', false);
+ib.reqRealTimeBars(14, ib.contract.stock('FB'), 5, 'MIDPOINT', false);
+
+// Option
+ib.reqRealTimeBars(21, ib.contract.option('AAPL', '201407', 500, 'C'), 5, 'TRADES', false);
+ib.reqRealTimeBars(22, ib.contract.option('AMZN', '201404', 350, 'P'), 5, 'BID', false);
+ib.reqRealTimeBars(23, ib.contract.option('GOOG', '201406', 1000, 'C'), 5, 'ASK', false);
+ib.reqRealTimeBars(24, ib.contract.option('FB', '201406', 50, 'P'), 5, 'MIDPOINT', false);
+
+// Unsubscribe after 15 seconds.
+setTimeout(function () {
+  console.log('Cancelling real-time bars subscription...'.yellow);
+
+  // Forex
+  // ib.cancelRealTimeBars(1);
+  // ib.cancelRealTimeBars(2);
+  // ib.cancelRealTimeBars(3);
+  // ib.cancelRealTimeBars(4);
+  // ib.cancelRealTimeBars(5);
+  // ib.cancelRealTimeBars(6);
+
+  //Stock
+  ib.cancelRealTimeBars(11);
+  ib.cancelRealTimeBars(12);
+  ib.cancelRealTimeBars(13);
+  ib.cancelRealTimeBars(14);
+
+  // Option
+  ib.cancelRealTimeBars(21);
+  ib.cancelRealTimeBars(22);
+  ib.cancelRealTimeBars(23);
+  ib.cancelRealTimeBars(24);
+}, 15000);
