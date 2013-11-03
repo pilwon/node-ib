6d828e9de05395b648090810f0f580cb0c4d2aa0
Pilwon Huh <pilwon@gmail.com>
2013-11-02 22:30:58 -0400

    Added reqAccountUpdates example.

---
+++ examples/reqAccountUpdates.js
@@ -0,0 +1,68 @@
+/*
+ * examples/reqAccountUpdates.js
+ */
+
+'use strict';
+
+require('colors');
+
+var _ = require('lodash');
+
+var ib = new (require('..'))({
+  // clientId: 0,
+  // host: '127.0.0.1',
+  // port: 7496
+}).on('error', function (err) {
+  console.error(err.message.red);
+}).on('result', function (event, args) {
+  if (!_.contains(['accountDownloadEnd', 'updateAccountTime', 'updateAccountValue', 'updatePortfolio'], event)) {
+    console.log('%s %s', (event + ':').yellow, JSON.stringify(args));
+  }
+}).on('accountDownloadEnd', function (accountName) {
+  console.log(
+    '%s %s%s',
+    '[accountDownloadEnd]'.cyan,
+    'accountName='.bold, accountName
+  );
+}).on('updateAccountTime', function (timeStamp) {
+  console.log(
+    '%s %s%s',
+    '[updateAccountTime]'.cyan,
+    'timeStamp='.bold, timeStamp
+  );
+}).on('updateAccountValue', function (key, value, currency, accountName) {
+  console.log(
+    '%s %s%s %s%s %s%s %s%s',
+    '[updateAccountValue]'.cyan,
+    'key='.bold, key,
+    'value='.bold, value,
+    'currency='.bold, currency,
+    'accountName='.bold, accountName
+  );
+}).on('updatePortfolio', function (contract, position, marketPrice, marketValue, averageCost, unrealizedPNL, realizedPNL, accountName) {
+  console.log(
+    '%s %s%s %s%d %s%d %s%d %s%d %s%d %s%d %s%s',
+    '[updatePortfolio]'.cyan,
+    'contract='.bold, JSON.stringify(contract),
+    'position='.bold, position,
+    'marketPrice='.bold, marketPrice,
+    'marketValue='.bold, marketValue,
+    'averageCost='.bold, averageCost,
+    'unrealizedPNL='.bold, unrealizedPNL,
+    'realizedPNL='.bold, realizedPNL,
+    'accountName='.bold, accountName
+  );
+});
+
+ib.connect();
+
+ib.reqAccountUpdates(true, 'U1234567');
+
+// Disconnect after 15 seconds.
+setTimeout(function () {
+  console.log('Cancelling account updates subscription...'.yellow);
+
+  ib.reqAccountUpdates(false, 'All');
+
+  ib.disconnect();
+}, 15000);
