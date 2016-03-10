bb8a782a4516c898b5ca66d0276a080a47ed9666
Guido Vettoretti <2guido@gmail.com>
2016-03-10 15:31:14 -0500

    Added reqHistoricalData example.

---
+++ examples/reqHistData.js
@@ -0,0 +1,43 @@
+require('colors');
+var _ = require('lodash');
+
+var ib = new (require('..'))({
+  // clientId: 0,
+  // host: '127.0.0.1',
+  // port: 7496
+}).on('error', function (err) {
+  console.error(err.message.red);
+}).on('historicalData', function (reqId, date, open, high, low, close, volume, barCount, WAP, hasGaps) {
+  if (_.contains([-1], open)) {
+    console.log('endhistoricalData');
+  } else {
+    console.log(
+    '%s %s%d %s%s %s%d %s%d %s%d %s%d %s%d %s%d %s%d %s%d',
+    '[historicalData]'.cyan,
+    'reqId='.bold, reqId,
+    'date='.bold, date,
+    'open='.bold, open,
+    'high='.bold, high,
+    'low='.bold, low,
+    'close='.bold, close,
+    'volume='.bold, volume,
+    'barCount='.bold, barCount,
+    'WAP='.bold, WAP,
+    'hasGaps='.bold, hasGaps
+    );
+  }
+});
+
+
+ib.connect();
+
+// tickerId, contract, endDateTime, durationStr, barSizeSetting, whatToShow, useRTH, formatDate
+ib.reqHistoricalData(1, ib.contract.stock('SPY','SMART','USD'), '20160308 12:00:00',durationStr='1800 S',barSizeSetting='1 secs',whatToShow='TRADES',useRTH=1,formatDate=1);  
+
+ib.on('historicalData', function (reqId, date, open, high, low, close, volume, barCount, WAP, hasGaps) {
+  if (_.contains([-1], open)) {
+    //ib.cancelHistoricalData(1);  // tickerId
+    ib.disconnect();
+  }
+});
+
